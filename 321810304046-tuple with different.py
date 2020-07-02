3b9620e606df88a8415fcb0a11eb42809a0d0931
gmp217 <66996033+gmp217@users.noreply.github.com>
2020-07-02 21:39:29 +0530

    Update 321810304046-tuple with different.py

--- 321810304046-tuple with different.py
+++ 321810304046-tuple with different.py
@@ -1,6 +1,2 @@
-l,n,t1=[],int(input('Enter value of n : ')),(2,3.8,'hello',True)
-for i in range (0,n):
-	ele=input()
-	l.append(ele)
-t=tuple(l)
-print('tuple t1:',t1,'\ntuple t:',t)
+t1=(2,3.8,'hello',True)
+print('tuple with different data types:',t1)
