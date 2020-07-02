b44b0b79832d62b1b72232e8e55939de02a76598
gmp217 <66996033+gmp217@users.noreply.github.com>
2020-07-02 21:37:26 +0530

    Update 321810304046-create tuple.py

--- 321810304046-create tuple.py
+++ 321810304046-create tuple.py
@@ -1,6 +1,2 @@
-l,n,t1=[],int(input('Enter value of n : ')),(2,3,5)
-for i in range (0,n):
-	ele=input()
-	l.append(ele)
-t=tuple(l)
-print('tuple t1:',t1,'\ntuple t:',t)
+t1=(2,3,5)
+print('tuple t1:',t1)
